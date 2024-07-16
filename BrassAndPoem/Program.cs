b4dea278225109d633bec2f0cd39ab0def1b6030
Taylor Seager <taylorseager718@gmail.com>
2024-07-15 19:55:42 -0500

    checking save

--- BrassAndPoem/Program.cs
+++ BrassAndPoem/Program.cs
@@ -1,8 +1,18 @@
-﻿
-//create a "products" variable here to include at least five Product instances. Give them appropriate ProductTypeIds.
+﻿List<Product> products = new List<Product>()
+{
+    new Product(name: "Trumpet", price: 50.75M, productTypeId: 1),
+    new Product(name: "Falling Up by Shel Silverstein", price: 15.99M, productTypeId: 2),
+    new Product(name: "The Final Flight by Unknown", price: 10.99M, productTypeId: 2),
+    new Product(name: "French Horn", price: 45.98M, productTypeId: 1),
+    new Product(name: "Tuba", price: 64.99M, productTypeId: 1),
+};
 
 //create a "productTypes" variable here with a List of ProductTypes, and add "Brass" and "Poem" types to the List. 
-
+List<ProductType> productTypes = new List<ProductType>()
+{
+    new ProductType(title: "Brass", id: 1),
+    new ProductType(title: "Poem", id: 2),
+};
 //put your greeting here
 
 //implement your loop here
